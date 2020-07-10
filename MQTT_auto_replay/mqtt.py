61581aab342a39dbf50a9ac1c7af1453d8f25722
RarceD <41490742+RarceD@users.noreply.github.com>
2020-07-10 19:38:26 +0200

    get 3 parameters and publish the update

--- MQTT_auto_replay/mqtt.py
+++ MQTT_auto_replay/mqtt.py
@@ -1,54 +1,70 @@
 
 #!/usr/bin/python
 import paho.mqtt.client as mqtt
-from not_show import *  # get the credentials of the devises
+# get the credentials of the devises
+from not_show import TOPICS_TO_SUBSCRIBE, HOST_MQTT, GET_INFO_DEVISES, TOPICS_TO_PUBLISH
+import sys
+import os
 
-# First I open the file:
-file = open("update.bin", "r")
-# I create the json
-json_update = file.read()
-# I close the file
-file.close()
+# I get the parameters:
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
 
-
-def on_connect(mqttc, obj, flags, rc):
-    pass
-    # print("rc: " + str(rc))
-
+business = ""  # The name of the enterprise to make the update
+devise = ""  # The model, or ESP32 or ATMEGA
+update_file_name = "" #The file direction of the update
 
 def on_message(mqttc, obj, msg):
+    pass
     # This is call when I receive a msg on a suscribe topic
+    """
     print('Topic: ' + msg.topic)
+    print('')
     print('Payload: ' + str(msg.payload))
-    print(' ')
-
+    print('')
+    print('')
+    """
     # print(msg.topic + msg + ":" + str(msg.payload))
     # mqttc.publish(TOPICS_TO_PUBLISH["SETUP"], GET_INFO_DEVISES['all_params'], 0, False)
 
 
-def on_publish(mqttc, obj, mid):
-    pass
-    # print("mid: " + str(mid))
-
-
-def on_subscribe(mqttc, obj, mid, granted_qos):
-    pass
-    #print("Subscribed: " + str(mid) + " " + str(granted_qos))
-
-
+# Check if the update is possible:
+if (len(sys.argv) != 4):
+    print("Invalid number of parameters !!")
+    sys.exit()
+else:
+    business = str(sys.argv[1]).upper()
+    devise = str(sys.argv[2]).upper()
+    update_file_name = str(sys.argv[3])
+    print(update_file_name)
+    #Check if it makes sense the words introduce:
+    if (business == 'INFRIKO' and (devise == 'ESP32' or devise == 'ATMEGA')):
+            print("Comienza el proceso de update...")
+    else:
+        print("Invalid parameters!!")
+        sys.exit()
+        
+# First I open the file:
+file = open(update_file_name, "r")
+# I create the json
+json_update = file.read()
+# I close the file
+file.close()
 # Configure the mqtt client:
 mqttc = mqtt.Client()
 mqttc.on_message = on_message
-mqttc.on_connect = on_connect
-mqttc.on_publish = on_publish
-mqttc.on_subscribe = on_subscribe
 # I connect to broker:
 mqttc.connect(HOST_MQTT, 1883, 60)
+# I publish the update:
+mqttc.publish('test',json_update, 0, False)
+
+"""
 # Suscribe to all the devises topics:
 mqttc.subscribe(TOPICS_TO_SUBSCRIBE['STATUS'], 0)
 mqttc.subscribe(TOPICS_TO_SUBSCRIBE['DATAS'], 0)
+mqttc.subscribe(TOPICS_TO_SUBSCRIBE['HELLO'], 0)
 mqttc.subscribe('topic', 0)
-mqttc.publish(TOPICS_TO_PUBLISH['SETUP'],
-              GET_INFO_DEVISES['all_params'], 0, False)
-
+# mqttc.publish(TOPICS_TO_PUBLISH['SETUP'],
+#               GET_INFO_DEVISES['all_params'], 0, False)
 mqttc.loop_forever()
+"""
