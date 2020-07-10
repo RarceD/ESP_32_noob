e0469eb07f77687be96b39edc031a8929f148603
RarceD <41490742+RarceD@users.noreply.github.com>
2020-07-10 17:45:18 +0200

    Get the update from a file and sent to broker

--- MQTT_auto_replay/create_json.py
+++
@@ -1,8 +0,0 @@
-import json 
-import not_show 
-
-# convert into JSON:
-y = json.dumps("hola", indent = 4)
-
-# the result is a JSON string:
-print(not_show.HOST_MQTT)
--- MQTT_auto_replay/mqtt.py
+++ MQTT_auto_replay/mqtt.py
@@ -1,6 +1,14 @@
 
+#!/usr/bin/python
 import paho.mqtt.client as mqtt
-from not_show import * ##get the credentials of the devises
+from not_show import *  # get the credentials of the devises
+
+# First I open the file:
+file = open("update.bin", "r")
+# I create the json
+json_update = file.read()
+# I close the file
+file.close()
 
 
 def on_connect(mqttc, obj, flags, rc):
@@ -9,19 +17,26 @@ def on_connect(mqttc, obj, flags, rc):
 
 
 def on_message(mqttc, obj, msg):
-    # print(msg.topic)
-    print(msg.topic + msg + ":" + str(msg.payload))
+    # This is call when I receive a msg on a suscribe topic
+    print('Topic: ' + msg.topic)
+    print('Payload: ' + str(msg.payload))
+    print(' ')
+
+    # print(msg.topic + msg + ":" + str(msg.payload))
     # mqttc.publish(TOPICS_TO_PUBLISH["SETUP"], GET_INFO_DEVISES['all_params'], 0, False)
 
 
 def on_publish(mqttc, obj, mid):
-    print("mid: " + str(mid))
+    pass
+    # print("mid: " + str(mid))
 
 
 def on_subscribe(mqttc, obj, mid, granted_qos):
     pass
-    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+    #print("Subscribed: " + str(mid) + " " + str(granted_qos))
 
+
+# Configure the mqtt client:
 mqttc = mqtt.Client()
 mqttc.on_message = on_message
 mqttc.on_connect = on_connect
@@ -29,10 +44,11 @@ mqttc.on_publish = on_publish
 mqttc.on_subscribe = on_subscribe
 # I connect to broker:
 mqttc.connect(HOST_MQTT, 1883, 60)
-#Suscribe to all the devises topics:
+# Suscribe to all the devises topics:
 mqttc.subscribe(TOPICS_TO_SUBSCRIBE['STATUS'], 0)
 mqttc.subscribe(TOPICS_TO_SUBSCRIBE['DATAS'], 0)
-mqttc.subscribe('a', 0)
-mqttc.publish(TOPICS_TO_PUBLISH["SETUP"], GET_INFO_DEVISES['all_params'], 0, False)
+mqttc.subscribe('topic', 0)
+mqttc.publish(TOPICS_TO_PUBLISH['SETUP'],
+              GET_INFO_DEVISES['all_params'], 0, False)
 
 mqttc.loop_forever()
